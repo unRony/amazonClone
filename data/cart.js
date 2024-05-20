7175e3790ff8a131ba3305a8ee60cfe42f8ce0f1
ruwith <ruwith0011@gmail.com>
2024-05-20 17:43:58 +0200

    mak

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,15 @@
 export const cart = [];
+
+export function addToCart(productId) {
+  let matchingItem;
+  cart.forEach((cartitem) => {
+    if (productId === cartitem.productId) {
+      matchingItem = cartitem;
+    }
+  });
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({ productId: productId, quantity: 1 });
+  }
+}
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,5 @@
 import { products } from "../data/products.js";
-import { cart } from "../data/cart.js";
+import { cart, addToCart } from "../data/cart.js";
 
 let productHtml = "";
 
@@ -61,25 +61,19 @@ products.forEach((product) => {
 
 document.querySelector(".js-products-grid").innerHTML = productHtml;
 
+function updateCartQuantity() {
+  let cartQuantity = 0;
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+  document.querySelector(".js-cart-quantity").textContent = `${cartQuantity}`;
+}
+
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
     const productId = button.dataset.productId;
-    let matchingItem;
-    cart.forEach((item) => {
-      if (productId === item.productId) {
-        matchingItem = item;
-      }
-    });
-    if (matchingItem) {
-      matchingItem.quantity += 1;
-    } else {
-      cart.push({ productId: productId, quantity: 1 });
-    }
 
-    let cartQuantity = 0;
-    cart.forEach((item) => {
-      cartQuantity += item.quantity;
-    });
-    document.querySelector(".js-cart-quantity").textContent = `${cartQuantity}`;
+    addToCart(productId);
+    updateCartQuantity();
   });
 });
