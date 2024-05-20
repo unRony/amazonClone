84e7a370991e3bbd582cd15bac0290636c0b6f90
ruwith <ruwith0011@gmail.com>
2024-05-21 00:51:04 +0200

    delete button

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -13,7 +13,9 @@ cart.forEach((cartItem) => {
     }
   });
 
-  cartSummaryHtml += `<div class="cart-item-container">
+  cartSummaryHtml += `<div class="cart-item-container js-cart-item-container-${
+    matchingProduct.id
+  }">
             <div class="delivery-date">Delivery date: Tuesday, June 21</div>
 
                <div class="cart-item-details-grid">
@@ -93,5 +95,9 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
     console.log(cart);
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    container.remove();
   });
 });
